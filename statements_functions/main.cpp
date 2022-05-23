5ffbba803474c7548a39452113045be5566c1885
silvererudite <shamima2hossain@gmail.com>
2022-05-23 18:54:23 +0600

    initial commit

---
+++ statements_functions/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+  int var1 = 2;
+  int var2 = 3;
+  std::cout << "Sum of nums: " << var1 + var2 << std::endl;
+  return 0; //to identify if the program ran correctly or not
+}
---
+++ template/main.cpp
@@ -0,0 +1,10 @@
+/*
+ Cannot nest comments
+ Type of errors: Compile time, Runtime, warnings
+*/
+#include <iostream>
+
+int main(){
+  std::cout << "hello world!" << std::endl;
+  return 0; //to identify if the program ran correctly or not
+}
